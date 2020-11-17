22efa15e474623b1dad451916742dd163d981545
GeneralSirMcDank <thomassluijs@live.nl>
2020-11-17 14:36:01 +0100

    Added BananaBoat

---
+++ ITCH.IO Project/Assets/Scripts/EnemyandAI/FoodShipAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodShipAI : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool Direction;
+    public float Speed;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Direction)
+        {
+            rb.MovePosition(rb.position + transform.up * Speed * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + transform.up * -Speed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Direction = !Direction;
+    }
+}
---
+++ ITCH.IO Project/Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float BaseSpeed;
+    public float Speed;
+    public bool IsGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        Movement();
+        Combat();
+    }
+
+
+    public void Combat()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            //Attack
+        }
+    }
+
+    public void Movement()
+    {
+        if (transform.position.y <= -2.8f)
+        {
+            IsGrounded = true;
+        }
+        else
+        {
+            IsGrounded = false;
+        }
+        if (!IsGrounded)
+        {
+            Speed = 2;
+        }
+        else
+        {
+            Speed = BaseSpeed;
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rb.MovePosition(rb.position + transform.right * Speed * Time.fixedDeltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rb.MovePosition(rb.position + transform.right * -Speed * Time.fixedDeltaTime);
+        }
+        if (IsGrounded == true && Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(0, 7, 0);
+        }
+    }
+}
