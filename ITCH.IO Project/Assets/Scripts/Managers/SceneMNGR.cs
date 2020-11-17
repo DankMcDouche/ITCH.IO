b1293a544233891bcc627a87306f197204039453
GeneralSirMcDank <thomassluijs@live.nl>
2020-11-17 12:27:35 +0100

    Added SceneManagement
    Added MainMenu/MainScene/GameoverScene

---
+++ ITCH.IO Project/Assets/Scripts/Managers/SceneMNGR.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMNGR : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+    public void StartMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void StartGameOverScene()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void StartMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
